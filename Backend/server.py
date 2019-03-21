d703e9d1a2feaf6608b4eb1823f6d05f9f69759e
Vitor Manoel da Silveira <vitormsilveira95@gmail.com>
2019-03-21 00:51:39 -0300

    Implementação do GET no endpoint collect

--- Backend/server.py
+++ Backend/server.py
@@ -35,72 +35,102 @@ class collect:
 
 ###################################    COLETA        ###################################
 
-@app.route('/collect',methods=['POST'])
+@app.route('/collect',methods=['POST', 'GET'])
 def receiveCollect():
-	print(request.json, file=sys.stderr)
-	if not request.json:
-		abort(400)
+	if request.method == "GET":
 		
-	col = collect(
-			request.json['collect'], 
-			request.json['gateway'], 
-			request.json['endpoint']
-			)
+		if not request.json:
+			abort(400)
+		
+		placeID = request.json["place_id"]
+		
+		dictionary = {}
 
-##########    Verifica se existe o endpoint e obtém o ID do Place      ##########
+		(cr, cnx) = openConnection()
+		query = "SELECT * FROM Collect WHERE Place_idPlace = (%s)"
+		dados = (placeID, )
+		
+		cr.execute(query, dados)
 
-	queryEndpoint = ("SELECT Place_idPlace FROM Endpoint WHERE mac = ('%s')" % (col.endpoint['mac'])) 
-	(cr,cnx) = openConnection()
-	cr.execute(queryEndpoint)	
-	if cr.rowcount == 0:
+		for collect in cr.fetchall():
+			dictionary.update({'collect_id': collect[0]})
+			dictionary.update({'accx': collect[1]})
+			dictionary.update({'accy': collect[2]})
+			dictionary.update({'accz': collect[3]})
+			dictionary.update({'girx': collect[4]})
+			dictionary.update({'giry': collect[5]})
+			dictionary.update({'girz': collect[6]})
+			dictionary.update({'temp': collect[7]})
+
 		closeConnection(cr, cnx)
-		return jsonify({'error': {'code': 1, 'description': 'Endpoint inexistente.'}})
+		
+		return jsonify({'collect': dictionary})
+		
 	else:
-		result = cr.fetchall()
-		idPlace = int(result[0][0])
-		closeConnection(cr, cnx)
-	closeConnection(cr, cnx)
-
+		print(request.json, file=sys.stderr)
+		if not request.json:
+			abort(400)
+			
+		col = collect(
+				request.json['collect'], 
+				request.json['gateway'], 
+				request.json['endpoint']
+				)
 	
-##########    Verifica se existe o gateway, se não, o cria      ##########
+	##########    Verifica se existe o endpoint e obtém o ID do Place      ##########
+	
+		queryEndpoint = ("SELECT Place_idPlace FROM Endpoint WHERE mac = ('%s')" % (col.endpoint['mac'])) 
+		(cr,cnx) = openConnection()
+		cr.execute(queryEndpoint)	
+		if cr.rowcount == 0:
+			closeConnection(cr, cnx)
+			return jsonify({'error': {'code': 1, 'description': 'Endpoint inexistente.'}})
+		else:
+			result = cr.fetchall()
+			idPlace = int(result[0][0])
+			closeConnection(cr, cnx)
+		closeConnection(cr, cnx)
+	
 		
-	queryGateway1 = ("SELECT idGateway FROM Gateway WHERE mac = ('%s')" % (col.gateway['mac'])) 
-	(cr,cnx) = openConnection()
-	cr.execute(queryGateway1)	
-	if cr.rowcount == 0:
-		queryGateway2 = ("INSERT INTO Gateway (mac) VALUES ('%s')" % (col.gateway['mac']))
-		try: 
-			cr.execute(queryGateway2)
-			cnx.commit()
-			cr.execute(queryGateway1)
+	##########    Verifica se existe o gateway, se não, o cria      ##########
+			
+		queryGateway1 = ("SELECT idGateway FROM Gateway WHERE mac = ('%s')" % (col.gateway['mac'])) 
+		(cr,cnx) = openConnection()
+		cr.execute(queryGateway1)	
+		if cr.rowcount == 0:
+			queryGateway2 = ("INSERT INTO Gateway (mac) VALUES ('%s')" % (col.gateway['mac']))
+			try: 
+				cr.execute(queryGateway2)
+				cnx.commit()
+				cr.execute(queryGateway1)
+				result = cr.fetchall()
+				idGateway = int(result[0][0])
+				closeConnection(cr, cnx)
+			except mysql.connector.Error as e: 
+				print(e, file=sys.stderr)
+				closeConnection(cr, cnx)
+				return jsonify({'error': {'code': e.errno, 'description': e.msg}})
+		else: 
 			result = cr.fetchall()
 			idGateway = int(result[0][0])
 			closeConnection(cr, cnx)
-		except mysql.connector.Error as e: 
+	
+	########## Grava no banco a coleta recebida do gateway         ##########
+	
+		date = time.strftime('%Y-%m-%d %H:%M:%S')
+		queryCollect = ("INSERT INTO Collect (accx, accy, accz, girx, giry, girz, temp, data, Gateway_idGateway, Place_idPlace) VALUES (%d, %d, %d, %d, %d, %d, %d, '%s', %d, %d)" % 
+			  (col.collect['accx'], col.collect['accy'], col.collect['accz'], col.collect['girx'], col.collect['giry'], col.collect['girz'], col.collect['temp'], date, idGateway, idPlace))
+		(cr,cnx) = openConnection()
+		try: 
+			cr.execute(queryCollect)
+			cnx.commit()
+			closeConnection(cr, cnx)
+		except mysql.connector.Error as e:
 			print(e, file=sys.stderr)
 			closeConnection(cr, cnx)
 			return jsonify({'error': {'code': e.errno, 'description': e.msg}})
-	else: 
-		result = cr.fetchall()
-		idGateway = int(result[0][0])
-		closeConnection(cr, cnx)
-
-########## Grava no banco a coleta recebida do gateway         ##########
-
-	date = time.strftime('%Y-%m-%d %H:%M:%S')
-	queryCollect = ("INSERT INTO Collect (accx, accy, accz, girx, giry, girz, temp, data, Gateway_idGateway, Place_idPlace) VALUES (%d, %d, %d, %d, %d, %d, %d, '%s', %d, %d)" % 
-		  (col.collect['accx'], col.collect['accy'], col.collect['accz'], col.collect['girx'], col.collect['giry'], col.collect['girz'], col.collect['temp'], date, idGateway, idPlace))
-	(cr,cnx) = openConnection()
-	try: 
-		cr.execute(queryCollect)
-		cnx.commit()
-		closeConnection(cr, cnx)
-	except mysql.connector.Error as e:
-		print(e, file=sys.stderr)
-		closeConnection(cr, cnx)
-		return jsonify({'error': {'code': e.errno, 'description': e.msg}})
-		
-	return jsonify({'OK': 201})
+			
+		return jsonify({'OK': 201})
 
 ########## EQUIPMENTS  ###############
 	
@@ -156,12 +186,12 @@ def places():
 			abort(400)
 			
 		description = request.json['description']
-		equipment_id = request.json['equipment_id']
+		equipmentID = request.json['equipment_id']
 
 		(cr, cnx) = openConnection()
 
 		query = "INSERT INTO Place (description, Equipment_idEquipment) VALUES (%s, %s)"
-		dados = (description, equipment_id)
+		dados = (description, equipmentID)
 
 		try: 
 			cr.execute(query, dados)
@@ -203,12 +233,12 @@ def endpoints():
 			abort(400)
 			
 		mac = request.json['mac']
-		place_id = request.json['place_id']
+		placeID = request.json['place_id']
 
 		(cr, cnx) = openConnection()
 
 		query = "INSERT INTO Endpoint (mac, Place_idPlace) VALUES (%s, %s)"
-		dados = (mac, place_id)
+		dados = (mac, placeID)
 
 		try: 
 			cr.execute(query, dados)
