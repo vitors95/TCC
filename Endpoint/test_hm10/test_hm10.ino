98d78ef65b0ad14133c6f47ce494c5d881a1fcd3
Vitor Manoel da Silveira <vitormsilveira95@gmail.com>
2019-03-21 23:49:54 -0300

    Alterado o programa do Endpoint para se adaptar ao Arduino Nano

--- Endpoint/test_hm10/test_hm10.ino
+++ Endpoint/test_hm10/test_hm10.ino
@@ -1,4 +1,7 @@
-//#include<Wire.h>
+#include<Wire.h>
+#include<SoftwareSerial.h>
+
+SoftwareSerial mySerial(3, 2); // RX, TX
 
 const int MPU_addr = 0x68; // Endereço I2C do MPU6050
 
@@ -6,57 +9,60 @@ int accx, accy, accz, girx, giry, girz, temp;
 
 void sendData(int data) {
   Serial.println(data, HEX);
-  Serial1.write(data);
+  mySerial.write(data);
 }
 
 void setup() {
   Serial.begin(9600);
-  Serial1.begin(9600);
+  mySerial.begin(9600);
 
-//  Wire.begin();
-//  Wire.beginTransmission(MPU_addr);
-//  Wire.write(0x6B);
-//  Wire.write(0);
-//  Wire.endTransmission(true);
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
 }
 
 void loop() {
-//  Wire.beginTransmission(MPU_addr);
-//  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-//  Wire.endTransmission(false);
-//  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-//  
-//  accx = Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)  
-//  Serial.println(accx);   
-//  accy = Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-//  Serial.println(accy);   
-//  accz = Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-//  Serial.println(accz);   
-//  girx = Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-//  Serial.println(girx);   
-//  giry = Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-//  Serial.println(giry);   
-//  girz = Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-//  Serial.println(girz);   
-//  temp = Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-//  Serial.println(temp);   
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  
+  accx = Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)  
+  accy = Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  accz = Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  girx = Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  giry = Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  girz = Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  temp = Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
 
-  accx = random(1, 100);
-  accx = 99;
-  accy = random(1, 100);
-  accz = random(1, 100);
-  girx = random(1, 100);
-  giry = random(1, 100);
-  girz = random(1, 100);
-  temp = random(1, 100);
+//  Serial.print("AcX = "); Serial.print(accx);
+//  Serial.print(" | AcY = "); Serial.print(accy);
+//  Serial.print(" | AcZ = "); Serial.print(accz);
+//  Serial.print(" | Tmp = "); Serial.print(temp/340.00+36.53);
+//  Serial.print(" | GyX = "); Serial.print(girx);
+//  Serial.print(" | GyY = "); Serial.print(giry);
+//  Serial.print(" | GyZ = "); Serial.println(girz);
 
-  sendData((int) accx);
-  sendData((int) accy);
-  sendData((int) accz);
-  sendData((int) girx);
-  sendData((int) giry);
-  sendData((int) girz);
-  sendData((int) temp);  
+  delay(1000);
 
-  delay(10000);
+  if (accx == 0 && accy == 0 && accz == 0) {
+    Wire.begin();
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x6B);
+    Wire.write(0);
+    Wire.endTransmission(true);
+  } else {
+    sendData((int) accx);
+    sendData((int) accy);
+    sendData((int) accz);
+    sendData((int) girx);
+    sendData((int) giry);
+    sendData((int) girz);
+    sendData((int) temp);  
+
+  }
+
+  delay(59000);
 }
