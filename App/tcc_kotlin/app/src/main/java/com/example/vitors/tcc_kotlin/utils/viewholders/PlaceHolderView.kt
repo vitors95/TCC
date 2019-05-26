a7adf8dce99257912dfcfdc272d66ec9eb48d427
Vitor Manoel da Silveira <vitormsilveira95@gmail.com>
2019-05-26 20:02:23 -0300

    Melhorias no app

--- App/tcc_kotlin/app/src/main/java/com/example/vitors/tcc_kotlin/utils/viewholders/PlaceHolderView.kt
+++ App/tcc_kotlin/app/src/main/java/com/example/vitors/tcc_kotlin/utils/viewholders/PlaceHolderView.kt
@@ -4,16 +4,13 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import com.example.vitors.tcc_kotlin.R
 import com.example.vitors.tcc_kotlin.models.Collect
 import com.example.vitors.tcc_kotlin.models.Place
 import com.example.vitors.tcc_kotlin.utils.helpers.DateHelper
 import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.components.YAxis
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.renderer.XAxisRenderer
 import com.github.mikephil.charting.utils.MPPointF
@@ -24,6 +21,9 @@ import kotlinx.android.synthetic.main.place_item.view.*
 import java.time.format.TextStyle
 import java.util.*
 import kotlin.collections.ArrayList
+import com.github.mikephil.charting.utils.EntryXComparator
+
+
 
 class PlaceHolderView(itemView: View): RecyclerView.ViewHolder(itemView) {
 
@@ -54,7 +54,11 @@ class PlaceHolderView(itemView: View): RecyclerView.ViewHolder(itemView) {
         }
     }
 
-    private fun setupXAxis(collects: Array<Collect>) {
+    private fun setupXAxisInTime(collects: Array<Collect>) {
+        resetScatterChart()
+        resetLineChart()
+        itemView.line_chart.visibility = View.VISIBLE
+
         val entries: ArrayList<Entry> = arrayListOf()
         val timestamps: ArrayList<Long> = arrayListOf()
 
@@ -63,7 +67,7 @@ class PlaceHolderView(itemView: View): RecyclerView.ViewHolder(itemView) {
             timestamps.add(dateHelper.dateString2Timetamp(collect.data))
         }
 
-        val dataSet = LineDataSet(entries, "Aceleração no eixo X (m/s²)")
+        val dataSet = LineDataSet(entries, "RMS x Pico a Pico")
         dataSet.fillAlpha = 1100
         dataSet.color = Color.RED
         dataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
@@ -113,7 +117,11 @@ class PlaceHolderView(itemView: View): RecyclerView.ViewHolder(itemView) {
         )
     }
 
-    private fun setupYAxis(collects: Array<Collect>) {
+    private fun setupYAxisInTime(collects: Array<Collect>) {
+        resetScatterChart()
+        resetLineChart()
+        itemView.line_chart.visibility = View.VISIBLE
+
         val entries: ArrayList<Entry> = arrayListOf()
         val timestamps: ArrayList<Long> = arrayListOf()
 
@@ -122,7 +130,7 @@ class PlaceHolderView(itemView: View): RecyclerView.ViewHolder(itemView) {
             timestamps.add(dateHelper.dateString2Timetamp(collect.data))
         }
 
-        val dataSet = LineDataSet(entries, "Aceleração no eixo Y (m/s²)")
+        val dataSet = LineDataSet(entries, "RMS x Pico a Pico")
         dataSet.fillAlpha = 1100
         dataSet.color = Color.RED
         dataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
@@ -172,7 +180,11 @@ class PlaceHolderView(itemView: View): RecyclerView.ViewHolder(itemView) {
         )
     }
 
-    private fun setupZAxis(collects: Array<Collect>) {
+    private fun setupZAxisInTime(collects: Array<Collect>) {
+        resetScatterChart()
+        resetLineChart()
+        itemView.line_chart.visibility = View.VISIBLE
+
         val entries: ArrayList<Entry> = arrayListOf()
         val timestamps: ArrayList<Long> = arrayListOf()
 
@@ -181,7 +193,7 @@ class PlaceHolderView(itemView: View): RecyclerView.ViewHolder(itemView) {
             timestamps.add(dateHelper.dateString2Timetamp(collect.data))
         }
 
-        val dataSet = LineDataSet(entries, "Aceleração no eixo Z (m/s²)")
+        val dataSet = LineDataSet(entries, "RMS x Pico a Pico")
         dataSet.fillAlpha = 1100
         dataSet.color = Color.RED
         dataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
@@ -199,7 +211,7 @@ class PlaceHolderView(itemView: View): RecyclerView.ViewHolder(itemView) {
         itemView.line_chart.axisLeft.setDrawGridLines(false)
         itemView.line_chart.axisRight.setDrawGridLines(false)
         itemView.line_chart.xAxis.setDrawGridLines(false)
-        itemView.line_chart.extraBottomOffset = 25f
+        itemView.line_chart.extraBottomOffset = 0f
 
         val xAxis = itemView.line_chart.xAxis
 
@@ -231,7 +243,128 @@ class PlaceHolderView(itemView: View): RecyclerView.ViewHolder(itemView) {
         )
     }
 
+    private fun setupXAxis(collects: Array<Collect>) {
+        resetLineChart()
+        resetScatterChart()
+        itemView.scatter_chart.visibility = View.VISIBLE
+
+        val entries: ArrayList<Entry> = arrayListOf()
+
+        collects.forEach { collect ->
+            entries.add(Entry(collect.accx.toFloat(), collect.rmsx.toFloat()))
+        }
+
+        Collections.sort(entries, EntryXComparator())
+
+        val dataSet = ScatterDataSet(entries, "RMS x Pico a Pico (eixo X)")
+        dataSet.color = Color.RED
+        dataSet.setDrawValues(false)
+        val scatterData = ScatterData(dataSet)
+
+        itemView.scatter_chart.data = scatterData
+        itemView.scatter_chart.description.text = ""
+        itemView.scatter_chart.legend.isEnabled = true
+        itemView.scatter_chart.invalidate()
+        itemView.scatter_chart.axisRight.isEnabled = false
+        itemView.scatter_chart.axisLeft.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART)
+        itemView.scatter_chart.axisRight.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART)
+        itemView.scatter_chart.axisLeft.setDrawGridLines(false)
+        itemView.scatter_chart.axisRight.setDrawGridLines(false)
+        itemView.scatter_chart.xAxis.setDrawGridLines(false)
+        itemView.scatter_chart.extraBottomOffset = 20f
+
+        val xAxis = itemView.scatter_chart.xAxis
+
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.labelCount = 4
+        xAxis.granularity = 1f
+        xAxis.isGranularityEnabled = true
+
+    }
+
+    private fun setupYAxis(collects: Array<Collect>) {
+        resetLineChart()
+        resetScatterChart()
+        itemView.scatter_chart.visibility = View.VISIBLE
+
+        val entries: ArrayList<Entry> = arrayListOf()
+
+        collects.forEach { collect ->
+            entries.add(Entry(collect.accy.toFloat(), collect.rmsy.toFloat()))
+        }
+
+        Collections.sort(entries, EntryXComparator())
+
+        val dataSet = ScatterDataSet(entries, "RMS x Pico a Pico (eixo Y)")
+        dataSet.color = Color.RED
+        dataSet.setDrawValues(false)
+        val scatterData = ScatterData(dataSet)
+
+        itemView.scatter_chart.data = scatterData
+        itemView.scatter_chart.description.text = ""
+        itemView.scatter_chart.legend.isEnabled = true
+        itemView.scatter_chart.invalidate()
+        itemView.scatter_chart.axisRight.isEnabled = false
+        itemView.scatter_chart.axisLeft.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART)
+        itemView.scatter_chart.axisRight.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART)
+        itemView.scatter_chart.axisLeft.setDrawGridLines(false)
+        itemView.scatter_chart.axisRight.setDrawGridLines(false)
+        itemView.scatter_chart.xAxis.setDrawGridLines(false)
+        itemView.scatter_chart.extraBottomOffset = 20f
+
+        val xAxis = itemView.scatter_chart.xAxis
+
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.labelCount = 4
+        xAxis.granularity = 1f
+        xAxis.isGranularityEnabled = true
+
+    }
+
+    private fun setupZAxis(collects: Array<Collect>) {
+        resetLineChart()
+        resetScatterChart()
+        itemView.scatter_chart.visibility = View.VISIBLE
+
+        val entries: ArrayList<Entry> = arrayListOf()
+
+        collects.forEach { collect ->
+            entries.add(Entry(collect.accz.toFloat(), collect.rmsz.toFloat()))
+        }
+
+        Collections.sort(entries, EntryXComparator())
+
+        val dataSet = ScatterDataSet(entries, "RMS x Pico a Pico (eixo Z)")
+        dataSet.color = Color.RED
+        dataSet.setDrawValues(false)
+        val scatterData = ScatterData(dataSet)
+
+        itemView.scatter_chart.data = scatterData
+        itemView.scatter_chart.description.text = ""
+        itemView.scatter_chart.legend.isEnabled = true
+        itemView.scatter_chart.invalidate()
+        itemView.scatter_chart.axisRight.isEnabled = false
+        itemView.scatter_chart.axisLeft.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART)
+        itemView.scatter_chart.axisRight.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART)
+        itemView.scatter_chart.axisLeft.setDrawGridLines(false)
+        itemView.scatter_chart.axisRight.setDrawGridLines(false)
+        itemView.scatter_chart.xAxis.setDrawGridLines(false)
+        itemView.scatter_chart.extraBottomOffset = 20f
+
+        val xAxis = itemView.scatter_chart.xAxis
+
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+        xAxis.labelCount = 4
+        xAxis.granularity = 1f
+        xAxis.isGranularityEnabled = true
+
+    }
+
     private fun setupTemp(collects: Array<Collect>) {
+        resetScatterChart()
+        resetLineChart()
+        itemView.line_chart.visibility = View.VISIBLE
+
         val entries: ArrayList<Entry> = arrayListOf()
         val timestamps: ArrayList<Long> = arrayListOf()
 
@@ -258,7 +391,7 @@ class PlaceHolderView(itemView: View): RecyclerView.ViewHolder(itemView) {
         itemView.line_chart.axisLeft.setDrawGridLines(false)
         itemView.line_chart.axisRight.setDrawGridLines(false)
         itemView.line_chart.xAxis.setDrawGridLines(false)
-        itemView.line_chart.extraBottomOffset = 25f
+        itemView.line_chart.extraBottomOffset = 20f
 
         val xAxis = itemView.line_chart.xAxis
 
@@ -288,6 +421,40 @@ class PlaceHolderView(itemView: View): RecyclerView.ViewHolder(itemView) {
                 itemView.line_chart.getTransformer(YAxis.AxisDependency.LEFT)
             )
         )
+    }
+
+    private fun resetLineChart() {
+        itemView.line_chart.fitScreen()
+        itemView.line_chart.data?.clearValues()
+        itemView.line_chart.xAxis.valueFormatter = null
+        itemView.line_chart.setXAxisRenderer(
+            XAxisRenderer(
+                itemView.line_chart.viewPortHandler,
+                itemView.line_chart.xAxis,
+                itemView.line_chart.getTransformer(YAxis.AxisDependency.LEFT)
+            )
+        )
+        itemView.line_chart.notifyDataSetChanged()
+        itemView.line_chart.clear()
+        itemView.line_chart.invalidate()
+        itemView.line_chart.visibility = View.INVISIBLE
+    }
+
+    private fun resetScatterChart() {
+        itemView.scatter_chart.fitScreen()
+        itemView.scatter_chart.data?.clearValues()
+        itemView.scatter_chart.xAxis.valueFormatter = null
+        itemView.scatter_chart.setXAxisRenderer(
+            XAxisRenderer(
+                itemView.scatter_chart.viewPortHandler,
+                itemView.scatter_chart.xAxis,
+                itemView.scatter_chart.getTransformer(YAxis.AxisDependency.LEFT)
+            )
+        )
+        itemView.scatter_chart.notifyDataSetChanged()
+        itemView.scatter_chart.clear()
+        itemView.scatter_chart.invalidate()
+        itemView.scatter_chart.visibility = View.INVISIBLE
     }
 
 }
